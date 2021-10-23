74aca17388663e7261493f51a9dbd5a214eb75f8
unknown <lpsk5713@gmail.com>
2021-10-24 04:46:25 +0530

    Sidebar Outlined

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,11 +1,47 @@
+import { style } from '@mui/system'
 import React from 'react'
+import styled from 'styled-components'
 
 function SideBar() {
     return (
-        <div>
-            <h1>I am Sidebar...</h1>
-        </div>
+        <Container>
+            <Logo>
+                <img src="https://cdn.vox-cdn.com/thumbor/Pkmq1nm3skO0-j693JTMd7RL0Zk=/0x0:2012x1341/1200x800/filters:focal(0x0:2012x1341)/cdn.vox-cdn.com/uploads/chorus_image/image/47070706/google2.0.0.jpg" alt="" />
+                <span>Community</span>
+            </Logo>
+            <Channels>
+                <a href="#">Channel 1</a>
+                <a href="#">Channel 2</a>
+                <a href="#">Channel 3</a>
+            </Channels>
+        </Container>
     )
 }
 
 export default SideBar
+
+
+const Container = styled.div`
+    height: 100vh;
+    width: 20vw;
+    background-color: white;
+`
+
+const Logo = styled.div`
+    display: flex;
+    img{
+        border: 2px solid black;
+        border-radius: 50%;
+        width: 30%;
+        height: 30%;
+    }
+    align-items: center;
+    justify-content: flex-start;
+    gap: 0 5%;
+`
+
+const Channels = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding: 5% 0% 0% 5%;
+`
